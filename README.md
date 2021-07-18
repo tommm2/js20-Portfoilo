a62f665b0e5d8e072023370d3cda3bb06dbffa76
tomm2 <tom875694231@gmail.com>
2021-07-18 16:25:34 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# JS20 Portfoilo
+
+功能:
+1. hover 到圖片上會有 github 連結和 Demo
+2. 動態時間 以及 收拉功能
+3. 歌曲播放、暫停
+4. 置頂功能
+5. RWD
+
+## [DEMO](https://tommm2.github.io/js20-portfoilo/)
+
+![img](https://i.postimg.cc/mDFM0T9r/image.jpg)
---
+++ script.js
@@ -0,0 +1,106 @@
+const projectLink = document.querySelectorAll('.project-info > a')
+const projectContainer = document.getElementById('project-container')
+const goTop = document.getElementById('go-top')
+const audioToggler = document.getElementById('audio-toggler')
+const audio = document.getElementById('audio')
+const timeContainer = document.getElementById('time-container')
+const time = document.getElementById('time')
+
+let data = []
+
+// set audio volume
+audio.volume = 0.4
+
+// Set date
+function setDate() {
+  var d = new Date();
+  var t = d.toLocaleString();
+  time.innerHTML = t;
+}
+
+
+setInterval(() => {
+  setDate()
+}, 1000)
+
+
+// store image 、 link data
+function storeData() {
+  for(let i = 1; i <= 20; i++) {
+    let prop = {
+      demo: `https://tommm2.github.io/js20-${i}/`,
+      github: `https://github.com/tommm2/js20-${i}`,
+      img: `./images/js20-${i}.JPG`
+    }
+    data.push(prop)
+  }
+}
+
+// Paused Song
+function pauseSong() {
+  audioToggler.classList.add('pause')
+  audio.pause()
+}
+
+// Play Song
+function playSong() {
+  audioToggler.classList.remove('pause')
+  audio.play()
+}
+
+// Updata DOM 
+function updateDOM() {
+  data.forEach((item, index) => {
+    const project = document.createElement('div')
+    project.setAttribute('class', 'project')
+    if(index === 14) {
+      project.style.backgroundImage = 'url("./images/no-provide.jpg")'
+      project.innerHTML = `
+        <div class="no-provide"><span>暫時沒做</span></div>
+      `
+    } else {
+      project.style.backgroundImage = `url('${item.img}')`
+      project.innerHTML = `
+        <div class="project-info">
+          <a href="${item.github}" target="_blank">Github</a>
+          <a href="${item.demo}" target="_blank">Demo</a>
+        </div>
+      `
+    }
+    projectContainer.appendChild(project)
+  })
+}
+
+storeData()
+updateDOM()
+
+// Event listener
+window.addEventListener('scroll', () => {
+  const { scrollTop } = document.documentElement;
+  if(scrollTop > 200) {
+    goTop.classList.add('show');
+  } else {
+    goTop.classList.remove('show')
+  }
+})
+
+goTop.addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  })
+})
+
+audioToggler.addEventListener('click', () => {
+  const isPause = audioToggler.classList.contains('pause')
+  if(isPause) {
+    playSong()
+  } else {
+    pauseSong()
+  }
+})
+
+timeContainer.addEventListener('click', () => {
+  timeContainer.classList.toggle('hide')
+})
